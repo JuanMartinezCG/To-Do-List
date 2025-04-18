86a416364723663459aca0b33265dcabed257941
JuanMartinezCG <oo199@outlook.om>
2025-04-18 02:59:12 -0500

    Se agrego el completado de alguna tarea y el boton de editar
    en Home.jsx se coloco las funciones de:
    handleToggleComplete (marcar tarea completada)
    toggleEditTask (cambia el estado de modo de la tarea para que aparezca un input pra su edicion )
    editTask (editar tarea con el texto nuevo que se esta colocando el input generado)

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TaskList = ({tasks, onDeleteTask}) => {
+const TaskList = ({tasks, onDeleteTask, onToggleComplete, onToggleEditTask, onEditTask}) => {
+  const [editingText, setEditingText] = useState("");
   return (
     <ul>
       {tasks.map(task => (
-        <li key={task.id}>
-            {task.text}
-            <button onClick={() => 
-                {console.log("Eliminando tarea con ID:", task.id);
-                onDeleteTask(task.id)}}>Eliminar</button>
+        <li key={task.id} className={task.completed ? 'completed' : ''}>
+          {task.editing ? (
+            <input
+                type="text"
+                value ={editingText}
+                onChange ={(e) => setEditingText(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    onEditTask(task.id, editingText); // para salir del modo edición
+                  }
+                }}
+                onBlur={() => {
+                  onEditTask(task.id, editingText); 
+                }}/>
+          ) : (
+            <>
+              <span onClick={() => onToggleComplete(task.id)}> {task.text} </span>
+              <button onClick={() =>{
+                onToggleEditTask(task.id)
+                setEditingText(task.text); // cargar el texto actual al input
+              }}> ✏️ Editar </button>
+              <button onClick={() =>{onDeleteTask(task.id)}}> ❌ Eliminar </button>
+            </>
+          )}
         </li>
       ))}
     </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import TaskList from '../components/TaskList';
 import AddTaskForm from '../components/AddTaskForm';
+// Importa los componentes necesarios para la página de inicio
 
 const Home = () => {
   const [tareas, setTareas] = useState([
-    { id: 1, text: 'Comprar leche' },
-    { id: 2, text: 'Estudiar React' },
+    { id: 1, text: 'Comprar leche' , completed: false, editing: false},
+    { id: 2, text: 'Estudiar React', completed: false, editing: false}
   ]);
   
   const AddTask = (texto) => { //agregar tarea
@@ -22,14 +23,39 @@ const Home = () => {
     setTareas(tareasActualizadas); // Actualiza el estado de tareas con las tareas filtradas
   }
 
+  const handleToggleComplete = (id) => { //marcar tarea como completada
+    const updateTask = tareas.map(task => 
+      task.id === id ? { ...task, completed: !task.completed } : task); // Cambia el estado de completado de la tarea seleccionada
+      setTareas(updateTask); // Actualiza el estado de tareas con la tarea modificada
+  }
+
+  const editTask  = (id, newTask) => { //editar tarea
+    const editingTask = tareas.map(task => 
+      task.id === id ? { ...task, text:newTask, editing: false } : task);
+    setTareas(editingTask); // Cambia el estado de edición de la tarea seleccionada
+  }
+
+  const toggleEditTask = (id) => {
+    const editingTask = tareas.map(task =>
+      task.id === id ? { ...task, editing: true} : task
+    );
+    setTareas(editingTask); // Cambia el estado de edición de la tarea seleccionada
+  };
+
   return (
     <div className="home">
       <h1>To Do List</h1>
       <AddTaskForm onAddTask={AddTask} />{/* Renderizamos el formulario y le pasamos la función para agregar tareas */}
-      <TaskList tasks={tareas}
-      onDeleteTask={DeleteTask} /> {/* Renderizamos la lista de tareas y le pasamos el array como prop */}
+      <TaskList /*Renderizamos la lista de tareas y le pasamos el array como prop */
+      tasks={tareas}
+      onDeleteTask={DeleteTask} // Renderizamos la lista de tareas y le pasamos la función para eliminar tareas
+      onToggleComplete={handleToggleComplete} // Renderizamos la lista de tareas y le pasamos la función para marcar tareas como completadas
+      onToggleEditTask={toggleEditTask} // Renderizamos la lista de tareas y le pasamos la función para editar tareas
+      onEditTask={editTask} // Renderizamos la lista de tareas y le pasamos la función para editar tareas
+      /> 
+      
     </div>
   );
 };
 
-export default Home;
+ export default Home;
